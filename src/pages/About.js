277b43b30c25880761015bb44d96f16c69e365d0
shoebm891 <shoebm891@gmail.com>
2023-07-21 19:39:35 +0530

    .

--- src/pages/About.js
+++ src/pages/About.js
@@ -21,7 +21,7 @@ const About = () => {
       <ScrollAnimation>
         <ScrollAnimation>
           <div class="container---">
-            <div className="player-wrapper">s
+            <div className="player-wrapper">
               <VideoSlider videos={videos} />
             </div>
             <ScrollAnimation>
@@ -29,73 +29,43 @@ const About = () => {
             </ScrollAnimation>
             <ScrollAnimation>
               <p>
-                Welcome to A Creations Farm Stay! Escape the hustle and bustle
-                of city life and experience the tranquility of our charming farm
-                rental. Nestled in a serene location, our farm stay offers a
-                perfect getaway for a relaxing and rejuvenating experience.
+                Welcome to White Hill Resort,
+                Where tranquility meets luxury's embrace.
+                Nestled amidst nature's scenic allure,
+                A haven for your soul to find solace.
               </p>
             </ScrollAnimation>
             <ScrollAnimation>
               <p>
-                Immerse yourself in the natural beauty that surrounds our farm.
-                Enjoy the picturesque landscapes, breathe in the fresh country
-                air, and reconnect with nature. Whether you're seeking a
-                peaceful retreat or an adventure-filled stay, A Creations Farm
-                Stay has something to offer for everyone.
+                Experience a stay beyond compare,
+                Where every moment turns into a dream.
+                From sun-kissed dawns to starlit air,
+                Unwind in opulence, it's more than it seems.
               </p>
             </ScrollAnimation>
             <ScrollAnimation>
               <p>
-                Our farm rental provides a range of amenities to ensure a
-                comfortable and enjoyable stay. Take a refreshing dip in the
-                swimming pool or engage in water games to beat the heat. Unwind
-                at the bar counter and savor a variety of drinks and cocktails
-                prepared by our skilled bartenders. Experience the joy of
-                outdoor grilling with our BBQ facilities and indulge in
-                flavorful meals cooked with coal.
+                Celebrate love with weddings divine,
+                With breathtaking backdrops for your special day.
+                Or host corporate events that truly shine,
+                In a setting that will take breaths away.
+
               </p>
             </ScrollAnimation>
             <ScrollAnimation>
               <p>
-                As the sun sets, gather around the bonfire and create
-                unforgettable memories with your loved ones. Explore the
-                surroundings on complimentary bicycles or engage in friendly
-                competition with a collection of board games available for your
-                entertainment.
+                For photographers seeking the perfect frame,
+                Capturing beauty here is an effortless feat.
+                And all your joyous celebrations, we aim,
+                To make them unforgettable and sweet.
               </p>
             </ScrollAnimation>
             <ScrollAnimation>
               <p>
-                Our well-equipped kitchen is at your disposal, complete with a
-                gas stove, utensils, and crockery. Prepare your favorite meals
-                using fresh ingredients and enjoy a homely dining experience.
-                Additionally, a microwave and double door refrigerator are
-                provided for your convenience.
-              </p>
-            </ScrollAnimation>
-            <ScrollAnimation>
-              <p>
-                We understand the importance of uninterrupted comfort during
-                your stay. In the event of a power outage, our generator ensures
-                a seamless experience.
-              </p>
-            </ScrollAnimation>
-            <ScrollAnimation>
-              <p>
-                For parties and corporate events, we offer tailored packages to
-                cater to your specific needs. With a capacity to accommodate up
-                to 100 guests, our farm stay provides a unique and picturesque
-                venue for your special occasions.
-              </p>
-            </ScrollAnimation>
-            <ScrollAnimation>
-              <p>
-                To get a glimpse of the beauty that awaits you at A Creations
-                Farm Stay, feel free to browse through our{" "}
-                <a href="https://drive.google.com/drive/folders/1-UmEzmp_RIxe1hCB-GrLmRy7WSHHtNnL">
-                  photo gallery
-                </a>
-                .
+                Open for bookings, our doors await,
+                Dial 📞 +91 9121699121 to reserve your stay.
+                Discover serenity at White Hill Resort,
+                Where cherished memories bloom and sway.
               </p>
             </ScrollAnimation>
             <ScrollAnimation>
@@ -103,52 +73,48 @@ const About = () => {
             </ScrollAnimation>
             <ScrollAnimation>
               <p>
-                We take pride in offering a wide range of amenities to make your
-                experience enjoyable and memorable. Here is a list of amenities
-                available:
+                Our resort boasts a wide array of amenities, carefully curated to cater to your every desire. From tranquil relaxation to exciting entertainment, you'll find it all amidst the nature.
+
               </p>
             </ScrollAnimation>
             <ul class="amenities-list">
               <ScrollAnimation>
                 <li>
-                  Swimming Pool: Take a refreshing dip in our sparkling swimming
-                  pool. Enjoy a relaxing swim or engage in water games to make
-                  the most of your time.
+                  Luxurious Bedrooms and Bathrooms:
+                  With five exquisitely designed bedrooms, your comfort is our top priority. Each room offers a cozy sanctuary to unwind and recharge. Accompanied by seven elegantly appointed bathrooms, you'll have all the space you need to refresh and rejuvenate.
+
                 </li>
               </ScrollAnimation>
               <ScrollAnimation>
                 <li>
-                  Bar Counter Setup: Unwind and socialize at our fully equipped
-                  bar counter. Enjoy a variety of drinks and cocktails prepared
-                  by our skilled bartenders.
+                  Spacious Living Rooms:
+                  Experience the joy of togetherness in our two large living rooms. Gather with your loved ones, bask in the warmth of the surroundings, and create cherished memories that will last a lifetime.
                 </li>
               </ScrollAnimation>
               <ScrollAnimation>
                 <li>
-                  BBQ (Coal): Experience the joy of outdoor grilling with our
-                  BBQ facilities. Cook up delicious meals using coal and enjoy a
-                  flavorful dining experience.
+                  Captivating Bar Setup with Amazing Views:
+                  Raise a toast to life's moments of joy at our well-equipped bar area. Marvel at the breathtaking vistas while sipping on your favorite beverages, making your stay truly unforgettable.
+
                 </li>
               </ScrollAnimation>
               <ScrollAnimation>
                 <li>
-                  Bonfire: Gather around a cozy bonfire and create unforgettable
-                  memories. Please note that there is an additional charge of
-                  ₹1500 for a two-hour bonfire session.
+                  Thrilling Pool Table:
+                  For those seeking entertainment, our resort offers a thrilling pool table that guarantees hours of friendly competition and fun-filled laughter.
+
                 </li>
               </ScrollAnimation>
               <ScrollAnimation>
                 <li>
-                  Cycling: Explore the surroundings on two wheels with our
-                  complimentary cycling facilities. Discover the area's scenic
-                  beauty while enjoying a leisurely ride.
+                  Expansive Ground Floor:
+                  The large ground floor space allows for various indoor activities, giving you the freedom to create your own memorable experiences.
                 </li>
               </ScrollAnimation>
               <ScrollAnimation>
                 <li>
-                  Water Games in Swimming Pool: Dive into the fun with our
-                  exciting water games in the swimming pool. Engage in
-                  activities that promise laughter and entertainment.
+                  Enchanting Open Theatre Area:
+                  Immerse yourself in the magic of cinema under the starry sky. Our open theatre area provides the perfect setting for unforgettable movie nights with your loved ones.
                 </li>
               </ScrollAnimation>
               <ScrollAnimation>
@@ -167,39 +133,35 @@ const About = () => {
               </ScrollAnimation>
               <ScrollAnimation>
                 <li>
-                  Microwave: Conveniently warm up snacks or heat leftovers using
-                  the microwave available in your accommodation.
+
                 </li>
               </ScrollAnimation>
               <ScrollAnimation>
                 <li>
-                  Double Door Refrigerator: Store your food and beverages in our
-                  spacious double door refrigerator. Keep your items fresh and
-                  easily accessible.
+
                 </li>
               </ScrollAnimation>
               <ScrollAnimation>
                 <li>
-                  Generator: In the event of a power outage, our generator
-                  ensures uninterrupted electricity supply, allowing you to
-                  enjoy your stay without any disruptions.
+
                 </li>
               </ScrollAnimation>
             </ul>
             <ScrollAnimation>
               <div class="contact---info">
                 <ScrollAnimation>
-                  <p>
-                    For bookings and further inquiries, please contact us at:
-                  </p>
-                  <p>Phone: 9393031001, 9390058561</p>
+                  <p>For bookings and further inquiries, please contact us at:</p>
                 </ScrollAnimation>
+                <ScrollAnimation>
+                  <p>Phone: +91 9121699121</p>
+                </ScrollAnimation>
+                <ScrollAnimation></ScrollAnimation>
                 <ScrollAnimation>
                   <h1 className="underlined-heading">LOCATION</h1>
                 </ScrollAnimation>
                 <ScrollAnimation>
                   <iframe
-                    src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d7620.205964181371!2d78.325506!3d17.262252!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bcbbf271a7aa60b%3A0x1fee920eb7355b98!2sA%20Creations%20Farm%20Stay!5e0!3m2!1sen!2sin!4v1689205088239!5m2!1sen!2sin"
+                    src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d30461.637258710034!2d78.22995!3d17.377939!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bcbeb4b49942561%3A0x773ea0cc350e33ac!2sWhite%20Hill%20Hyd!5e0!3m2!1sen!2sin!4v1689942594177!5m2!1sen!2sin"
                     width="100%"
                     height="400"
                     style={{ border: "0" }}
